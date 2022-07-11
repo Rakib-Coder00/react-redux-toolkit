54bd5672ced9e1714beb4aa787f4a1f9c20b6f40
unknown <rhbappy456@gmail.com>
2022-07-11 09:15:55 +0600

    5 increment & decrement with dispatch

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
           <h1>Increment/Decrement counter</h1>
           <h4>using React and Redux</h4>
           <div className="quantity">
-            <a onClick={()=> dispatch(decNumber())} className="quantity__minus" title="Decrement" ><span>-</span></a>
+            <a onClick={()=> dispatch(decNumber(5))} className="quantity__minus" title="Decrement" ><span>-</span></a>
             <input name="quantity" type="text" className="quantity__input" value={myState}/>
-            <a onClick={()=> dispatch(incNumber())} className="quantity__plus" title="Increment"><span>+</span></a>
+            <a onClick={()=> dispatch(incNumber(5))} className="quantity__plus" title="Increment"><span>+</span></a>
           </div>
         </div>
       </div>
