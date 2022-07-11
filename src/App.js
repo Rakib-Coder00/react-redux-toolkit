6711ac14228eb18566c7a76e4ddc6467cc128069
unknown <rhbappy456@gmail.com>
2022-07-11 09:00:05 +0600

    create redux store & rootreducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 
+
 function App() {
+  const myState = useSelector(state => state.changeTheNumber);
   return (
     <>
       <div className="main-div">
-        <div class="container">
+        <div className="container">
           <h1>Increment/Decrement counter</h1>
           <h4>using React and Redux</h4>
-          <div class="quantity">
-            <a class="quantity__minus" title="Decrement" ><span>-</span></a>
-            <input name="quantity" type="text" class="quantity__input" value='0' />
-            <a class="quantity__plus" title="Increment"><span>+</span></a>
+          <div className="quantity">
+            <a className="quantity__minus" title="Decrement" ><span>-</span></a>
+            <input name="quantity" type="text" className="quantity__input" value={myState}/>
+            <a className="quantity__plus" title="Increment"><span>+</span></a>
           </div>
         </div>
       </div>
